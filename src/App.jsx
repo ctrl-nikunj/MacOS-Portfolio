d5f6f127054ee9911e422d2fd349c00f5bea2128
Nikunj Goyal <goyalnikunj2004@gmail.com>
2025-11-22 14:02:53 +0530

    feat: Introduce an animated Dock component with tooltips and integrate it into the main application, adding `react-tooltip` dependency.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Navbar";
 import Welcome from "@/components/Welcome";
+import Dock from "@/components/Dock";
 
 function App() {
   return (
     <main>
       <Navbar />
       <Welcome />
+      <Dock />
     </main>
   );
 }
